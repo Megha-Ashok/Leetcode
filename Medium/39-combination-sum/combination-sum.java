e7e15ffedab947ffb05de43d1cc392214fe69837
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-05-30 11:43:50 +0530

    Time: 1 ms (99.83%) | Memory: 44.8 MB (31.99%) - LeetSync

--- Medium/39-combination-sum/combination-sum.java
+++ Medium/39-combination-sum/combination-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     static List<List<Integer>>arr1;
+    //added
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
     arr1=new ArrayList<>();
     ArrayList<Integer>arr=new ArrayList<>();

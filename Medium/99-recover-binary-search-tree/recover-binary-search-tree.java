15c347a9620fb8749ab5af161ec5387c3b1135eb
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-05-30 17:33:22 +0530

    Time: 3 ms (11.68%) | Memory: 44.7 MB (33.29%) - LeetSync

---
+++ Medium/99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static ArrayList<Integer>arr;
+    static TreeNode cur;
+    static int i;
+    public void recoverTree(TreeNode root) {
+        arr=new ArrayList<>();
+        callfun(root);
+        Collections.sort(arr);
+        cur=root;
+        i=0;
+        callfun1(root);
+    }
+    public static void callfun(TreeNode root){
+        if(root==null)return;
+        callfun(root.left);
+        arr.add(root.val);
+        callfun(root.right);
+    }
+    public static void callfun1(TreeNode root){
+         if(root==null)return;
+        callfun1(root.left);
+        if(root.val!=arr.get(i)){
+            root.val=arr.get(i++);
+        }
+        else{
+            i++;
+        }
+        callfun1(root.right);
+    }
+}

37dc0b6345ef75e8da94a8803d3bce7b3f8266ae
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-05-30 12:11:58 +0530

    Time: 725 ms (5.27%) | Memory: 45.7 MB (6.34%) - LeetSync

---
+++ Medium/47-permutations-ii/permutations-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    static List<List<Integer>>res;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        res=new ArrayList<>();
+        ArrayList<Integer>arr=new ArrayList<>();
+        callfun(nums,arr);
+        return res;
+    }
+    public static void callfun(int nums[],ArrayList<Integer>arr){
+        if(arr.size()==nums.length){
+             ArrayList<Integer>temp=new ArrayList<>();
+             for(int i=0;i<nums.length;i++){
+                 temp.add(nums[arr.get(i)]);
+             }
+            if(!res.contains(temp)){
+                res.add(new ArrayList<>(temp));
+            }
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!arr.contains(i)){
+                arr.add(i);
+                callfun(nums,arr);
+                arr.remove(arr.size()-1);
+            }
+        }   
+    }
+}

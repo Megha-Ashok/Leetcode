e692378e41d72bd242bf16d9787c80e5fecca2fd
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-06-06 09:01:14 +0530

    Time: 3 ms (100.00%) | Memory: 57.6 MB (63.91%) - LeetSync

--- Medium/2455-node-with-highest-edge-score/node-with-highest-edge-score.java
+++ Medium/2455-node-with-highest-edge-score/node-with-highest-edge-score.java
@@ -1,23 +1,16 @@
 class Solution {
     public int edgeScore(int[] edges) {
-        HashMap<Long,Long>hash=new HashMap<>();
-        long max=0;
-        for(int i=0;i<edges.length;i++){
-          if(hash.containsKey((long)edges[i])){
-            long val=hash.get((long)edges[i]);
-            hash.put((long)edges[i],val+i);
-            max=Math.max(max,val+i);
-          }
-          else{
-            hash.put((long)edges[i],(long)i);
-            max=Math.max(max,i);
-          }
+      int n=edges.length;
+        long arr[]=new long[n];
+        for(int i=0;i<n;i++){
+          arr[edges[i]]+=i;
         }
-        long res=edges.length+1;
-        for(Map.Entry<Long,Long>set:hash.entrySet()){
-         if(max==set.getValue()){
-          res=Math.min(res,set.getKey());
-         }
+        long max=0,res=0;
+        for(int i=0;i<n;i++){
+          if(arr[i]>max){
+            res=i;
+            max=arr[i];
+          }
         }
         return (int)res;
     }

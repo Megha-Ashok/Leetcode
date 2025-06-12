93c513b0ea635a0a9119d5cd7d54f504a59d1df7
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-06-12 12:00:47 +0530

    Time: 5 ms (98.40%) | Memory: 43.6 MB (30.03%) - LeetSync

--- Medium/179-largest-number/largest-number.java
+++ Medium/179-largest-number/largest-number.java
@@ -4,7 +4,7 @@ class Solution {
       if(n==1)return ""+nums[0];
       String str[]=new String[n];
       for(int i=0;i<n;i++){
-        str[i]=Integer.toString(nums[i]);
+        str[i]=nums[i]+"";
       }
       Arrays.sort(str,(a,b)->(b+a).compareTo(a+b));
       StringBuilder res=new StringBuilder();

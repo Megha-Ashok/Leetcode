0a18b3c18692eca59b16bf7b2052597018a213cc
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-06-28 22:42:41 +0530

    Time: 6 ms (37.72%) | Memory: 68.2 MB (33.75%) - LeetSync

--- Medium/2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ Medium/2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,31 +1,14 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-       ArrayList<Integer>lessVal=new ArrayList<>();
-       ArrayList<Integer>greaterVal=new ArrayList<>();
-       ArrayList<Integer>pivotVal=new ArrayList<>();
-       int res[]=new int[nums.length];
-       for(int val:nums){
-        if(val>pivot){
-          greaterVal.add(val);
-        }
-        else if(val<pivot){
-          lessVal.add(val);
-        }
-        else{
-          pivotVal.add(pivot);
-        }
-       } 
-      int k=0;
-      for(int i=0;i<lessVal.size();i++){
-        res[k++]=lessVal.get(i);
-      }
-      
-      for(int i=0;i<pivotVal.size();i++){
-        res[k++]=pivotVal.get(i);
-      }
-      for(int i=0;i<greaterVal.size();i++){
-        res[k++]=greaterVal.get(i);
-      }
+      int n=nums.length;
+       int left=0,right=n-1;
+       int res[]=new int[right+1];
+       Arrays.fill(res,pivot);
+       for(int i=0;i<n;i++){
+        if(nums[i]<pivot)res[left++]=nums[i];
+        if(nums[n-i-1]>pivot)res[right--]=nums[n-i-1];
+       }
+    
       return res;
     }
 }

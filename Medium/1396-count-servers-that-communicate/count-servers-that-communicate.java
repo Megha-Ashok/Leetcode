bc35c5e32a1c5be31d69d07e8c1aa128a2295f08
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-06-29 12:20:16 +0530

    Time: 2 ms (99.59%) | Memory: 52.2 MB (75.93%) - LeetSync

--- Medium/1396-count-servers-that-communicate/count-servers-that-communicate.java
+++ Medium/1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -1,35 +1,21 @@
 class Solution {
     public int countServers(int[][] grid) {
-      PriorityQueue<int[]>row=new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
-      PriorityQueue<int[]>col=new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
+        int col[]=new int[grid[0].length];
+        int row[]=new int[grid.length];
       for(int i=0;i<grid.length;i++){
         for(int j=0;j<grid[i].length;j++){
             if(grid[i][j]==1){
-                row.add(new int[]{i,j});
-                col.add(new int[]{j,i});
+                row[i]++;
+                col[j]++;
             }
-
-        }
-      }
-      int visit[][]=new int[grid.length][grid[0].length];
-      while(!row.isEmpty()){
-        int arr[]=row.remove();
-        if(!row.isEmpty() && row.peek()[0]==arr[0]){
-            visit[row.peek()[0]][row.peek()[1]]=1;
-            visit[arr[0]][arr[1]]=1;
-        }
-      }
-      while(!col.isEmpty()){
-        int arr[]=col.remove();
-        if(!col.isEmpty() && col.peek()[0]==arr[0]){
-            visit[col.peek()[1]][col.peek()[0]]=1;
-            visit[arr[1]][arr[0]]=1;
         }
       }
       int count=0;
       for(int i=0;i<grid.length;i++){
-        for(int j=0;j<grid[0].length;j++){
-          if(visit[i][j]==1)count++;
+        for(int j=0;j<grid[i].length;j++){
+            if(grid[i][j]==1 && (row[i]>1 || col[j]>1)){
+               count++;
+            }
         }
       }
       return count;

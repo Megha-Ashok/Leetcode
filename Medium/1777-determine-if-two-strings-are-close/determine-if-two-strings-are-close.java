964182ce4877a76682f3615e453160223fc4d91c
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-07-18 06:47:29 +0530

    Time: 12 ms (55.81%) | Memory: 45.7 MB (52.01%) - LeetSync

--- Medium/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ Medium/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -8,36 +8,12 @@ class Solution {
            ch1[word1.charAt(i)-'a']++;  
            ch2[word2.charAt(i)-'a']++;
         }
-        HashMap<Integer,Integer>hash1=new HashMap<>();
-        HashMap<Integer,Integer>hash2=new HashMap<>();
         for(int i=0;i<26;i++){
-        if(ch1[i]==0){
-          if(ch2[i]!=0)return false;
+          if((ch1[i]!=0 && ch2[i]!=0)|| (ch1[i]==0 && ch2[i]==0))continue;
+          return false;
         }
-        if(ch2[i]==0){
-          if(ch1[i]!=0)return false;
-        }
-        if(ch1[i]!=0){
-          if(hash1.containsKey(ch1[i])){
-            hash1.put(ch1[i],hash1.get(ch1[i])+1);
-          }
-          else{
-            hash1.put(ch1[i],1);
-          }
-        }
-        if(ch2[i]!=0){
-          if(hash2.containsKey(ch2[i])){
-            hash2.put(ch2[i],hash2.get(ch2[i])+1);
-          }
-          else{
-            hash2.put(ch2[i],1);
-          }
-        }
-        }
-        for(Map.Entry<Integer,Integer>set:hash1.entrySet()){
-          if(hash2.containsKey(set.getKey()) && set.getValue()!=hash2.get(set.getKey()))return false;
-          if(!hash2.containsKey(set.getKey()))return false;
-        }
-        return true;
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return Arrays.equals(ch1,ch2);
     }
 }

d6ae4e37c517026dd74f2acc8dd40147315b2011
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-07-25 10:26:08 +0530

    Time: 34 ms (34.34%) | Memory: 58 MB (14.59%) - LeetSync

--- Medium/2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
+++ Medium/2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
@@ -1,14 +1,13 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
-        int l=0,r=0,result=0;
-        while(r<nums.length){
-          while(r<nums.length && nums[r]-nums[l]<=k){
-            r++;
-          }
+        int l=0,result=0;
+       for(int i=1;i<nums.length;i++){
+        if(nums[i]-nums[l]>k){
           result++;
-          l=r;
+          l=i;
         }
-        return result;
+       }
+        return result+1;
     }
 }

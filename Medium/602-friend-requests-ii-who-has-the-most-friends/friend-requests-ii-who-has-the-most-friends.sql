dfa1c952352a124b43dd181ab9da7cedd3fe8394
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-07-31 22:41:23 +0530

    Time: 268 ms (97.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ Medium/602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,2 @@
+select id,count(*) as num
+from (select accepter_id as id from RequestAccepted union all select requester_id as id from RequestAccepted)as allids group by id order by num desc limit 1;  

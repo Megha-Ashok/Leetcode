6ed26658762b2662efceb93d2a2a929525347b0d
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-08-14 21:53:52 +0530

    Time: 2 ms (26.20%) | Memory: 41.3 MB (54.55%) - LeetSync

---
+++ Medium/1010-powerful-integers/powerful-integers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+       TreeSet<Integer>res=new TreeSet<>();
+       for(int i=1;i<bound;i*=x){
+        for(int j=1;i+j<=bound;j*=y){
+            res.add(i+j);
+            if(y==1)break;
+        }
+        if(x==1)break;
+       }
+       return new ArrayList<>(res);
+    }
+   
+}

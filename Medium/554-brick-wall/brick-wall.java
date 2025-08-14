881296a7129fa4452978eebeb7b47d6f4628e3b7
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-08-14 23:00:28 +0530

    Time: 90 ms (5.01%) | Memory: 48.4 MB (50.40%) - LeetSync

---
+++ Medium/554-brick-wall/brick-wall.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+      HashMap<Integer,Integer>hash=new HashMap<>();
+      for(int i=0;i<wall.size();i++){
+        int sum=0;
+        for(int j=0;j<wall.get(i).size()-1;j++){
+          sum+=wall.get(i).get(j);
+          if(hash.containsKey(sum)){
+            hash.put(sum,hash.get(sum)+1);
+          }
+          else{
+            hash.put(sum,1);
+          }
+        }
+      }
+      int max=0;
+      for(Map.Entry<Integer,Integer>set:hash.entrySet()){
+        System.out.println(set.getKey()+" "+set.getValue());
+        max=Math.max(max,set.getValue());
+      }
+      return wall.size()-max;
+    }
+}

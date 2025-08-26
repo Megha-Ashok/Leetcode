81bf24b05f01b9c6ebc02d631f5f53892c2859ce
Megha Ashok Rabagannavar <160044256+Megha-Ashok@users.noreply.github.com>
2025-08-26 07:49:53 +0530

    Time: 47 ms (73.29%) | Memory: 55.3 MB (51.04%) - LeetSync

--- Medium/940-fruit-into-baskets/fruit-into-baskets.java
+++ Medium/940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,46 +1,33 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-    int first=-1,second=-1;
-    int count1=0,count2=0;
-    int i=0,j=0;
-    int n=fruits.length,max=0;
-    while(i<n){
-     if(fruits[i]!=first && fruits[i]!=second){
-      if(first==-1){
-        first=fruits[i];
-        count1=1;
-      }
-      else if(second==-1){
-        second=fruits[i];
-        count2=1;
-      }
-      else{
-        max=Math.max(max,count1+count2);
-        while(j!=i){
-         if(fruits[j]==first)count1--;
-         else if(fruits[j]==second)count2--;
-         if(count1==0 || count2==0)break;
-         j++;
-        }
-        if(count1==0){
-          first=fruits[i];
-          count1=1;
+       int left=0,right=0;
+       int n=fruits.length;
+       HashMap<Integer,Integer>set=new HashMap<>();
+       int len=0;
+       while(right<n){
+        if(set.containsKey(fruits[right])){
+        set.put(fruits[right],set.get(fruits[right])+1);
         }
         else{
-          second=fruits[i];
-          count2=1;
+            if(set.size()==2){
+                while(left<=right){
+                  int value=set.get(fruits[left]);
+                  if(value==1){
+                    set.remove(fruits[left]);
+                    left++;
+                    break;
+                  }
+                  else{
+                    set.put(fruits[left],value-1);
+                  }
+                  left++;
+                }
+            }
+            set.put(fruits[right],1);
         }
-      }
-     }
-     else if(fruits[i]==first){
-     count1++;
-     }
-     else{
-      count2++;
-     }
-     i++;
-    }
-    max=Math.max(max,count1+count2);
-    return max;
+        len=Math.max(len,right-left+1);
+        right++;
+       }
+       return len;
     }
 }
